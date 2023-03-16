107b6553b929c80e7c8f3f2328e6cc9e70a3547f
LordKoff-cmd <a46052@alunos.isel.pt>
2023-03-16 15:43:27 +0000

    autorouterReflect() start impl

--- autorouter/src/main/java/pt/isel/autorouter/AutoRouterReflect.java
+++ autorouter/src/main/java/pt/isel/autorouter/AutoRouterReflect.java
@@ -1,12 +1,30 @@
 package pt.isel.autorouter;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class AutoRouterReflect {
     public static Stream<ArHttpRoute> autorouterReflect(Object controller) {
-        /**
-         * TO DO
-         */
+        // Only to get methods or all declaredMembers?
+        // TODO: Should the controller inputs be public or not?
+
+        var controllerClass = controller.getClass();
+
+        for (Method m : controllerClass.getDeclaredMethods()){
+            System.out.println("Method: " + m.getName());
+            System.out.println("Parameters: ");
+            for(Parameter p : m.getParameters()) System.out.println("\t" + p);
+            for(Annotation a : m.getDeclaredAnnotations()) System.out.println("\t" + a);
+            System.out.println("Return Type: " + m.getReturnType());
+            System.out.println("---+---+---+---+---+---");
+        }
         return Stream.empty();
     }
 }
