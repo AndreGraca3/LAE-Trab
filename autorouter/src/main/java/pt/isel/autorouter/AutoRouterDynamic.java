3d4a0b60dffdae9f73967ab1454885d7f65dd84f
LordKoff-cmd <a46052@alunos.isel.pt>
2023-04-17 22:06:24 +0100

    First commit of dynamic implementation -> autorouterDynamic()

--- autorouter/src/main/java/pt/isel/autorouter/AutoRouterDynamic.java
+++ autorouter/src/main/java/pt/isel/autorouter/AutoRouterDynamic.java
@@ -1,6 +1,9 @@
 package pt.isel.autorouter;
 
+import kotlin.jvm.internal.DefaultConstructorMarker;
 import org.cojen.maker.ClassMaker;
+import org.cojen.maker.FieldMaker;
+import org.cojen.maker.MethodMaker;
 import pt.isel.autorouter.annotations.AutoRoute;
 
 import java.lang.reflect.Method;
@@ -24,9 +27,8 @@ public class AutoRouterDynamic {
                         return new ArHttpRoute(method.getName(), verb, path, handler);
                     } catch (ReflectiveOperationException ex) {
                         System.err.println("Error creating handler for method " + method.getName() + ": " + ex.getMessage());
-                        return null;
                     }
-
+                    return null;
                 });
     }
 
@@ -35,23 +37,53 @@ public class AutoRouterDynamic {
                 .public_()
                 .implement(ArHttpHandler.class);
 
+        // Add the field that holds the controller instance
+        FieldMaker controller = cm.addField(controllerClass, "controller")
+                .private_()
+                .final_();
+
         // Add the public constructor that takes the controller class as a parameter
-        cm.addConstructor()
+//        cm.addMethod(void.class, "<init>", controllerClass)
+//                .public_()
+//                .param(0).set(controller);
+
+        cm.addConstructor(controllerClass)
                 .public_()
-                .param(controllerClass)
-                .body()
-                .callSuper()
-                .return_();
+                .param(0).set(controller);
+
+
+        // Create the handle method
+        MethodMaker handle = cm.addMethod(Optional.class, "handle", Map.class, Map.class, Map.class)
+                .public_();
+
+        // Get the parameters of the method
+        Class<?>[] parameters = method.getParameterTypes();
+//        for (Class<?> parameter : parameters) {
+//            if(parameter.isPrimitive()){
+//                System.out.println("Primitive");
+//            } else {
+//                System.out.println("Not primitive");
+//            }
+//        }
+
+
+
+
+
+
+
+        // Add the code to call the controller method
+//        handle.var(0).invoke(method.getName(),);
+
 
         return cm;
     }
 
 
-
-
 //    public class SearchClassroomControllerClass implements ArHttpHandler {
 //
 //        private final ClassroomController controller;
+//
 //        public SearchClassroomControllerClass(ClassroomController controller) {
 //            this.controller = controller;
 //        }
